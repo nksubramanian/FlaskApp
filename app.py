9e1bef80073a0d9498885539eae6189a3848daa7
subramaniannk <58900536+subramaniannk@users.noreply.github.com>
2021-03-16 15:36:50 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/<name>')
+def name(name):
+    return 'Hello' + name
+
+if __name__=='__main__':
+    app.run(debug=True)
